5bb30637b17cab25039b16054f8eab219ebce4a0
Aditya Valsangkar <adidev2550@gmail.com>
2026-02-24 11:48:47 +0530

    Handling unhandled routes and requests

--- app.js
+++ app.js
@@ -14,11 +14,6 @@ app.use(express.json()); //middleware
 app.use(express.static(`${__dirname}/public`));
 
 app.use((req, res, next) => {
-  console.log('Hello from the middleware👋');
-  next();
-}); // creating and using our own middleware
-
-app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
@@ -28,4 +23,11 @@ app.use((req, res, next) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
+// Handling a request (bad req)
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 module.exports = app;
