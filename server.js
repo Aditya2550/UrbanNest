973d68c88918bf6ee93efe163641df4194b016b2
Aditya Valsangkar <adidev2550@gmail.com>
2026-05-07 12:54:11 +0530

    Handling unhandled rejection errors

--- server.js
+++ server.js
@@ -14,17 +14,24 @@ mongoose
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
-    useUnifiedTopology: true,
   })
   .then(() => {
-    console.log('DB connection successful!');
-    console.log('Connected to DB:', mongoose.connection.name);
+    console.log('DB connection successful!')
   });
+
 // .catch((err) => console.error('❌ DB connection error:', err.message));
 
 //CREATING MODEL FOR SCHEMA (ALWAYS NAME OF MODEL STARTS WITH CAPITAL ALPHABET)
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`App runnig on port ${port}...`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log(err.name, err.message);
+  console.log('UNHANDLER REJECTION! 💥 SHUTTING down...');
+  server.close(() => {
+    process.exit(1);
+  });
 });
